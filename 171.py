f3faed4ef6abfc05cf1034d444f1a9b7057ddc4d
Shufan <pelvoc@gmail.com>
2018-08-02 23:02:59 -0700

    E32

---
+++ 171.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res=0
+    
+        for i in range(len(s)):
+            
+            res=res*26+(ord(s[i])-ord('A'))+1
+            
+        return res 
