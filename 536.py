6511e1228e7abc66785b62d48cf0cb96590b5b83
Shufan <pelvoc@gmail.com>
2018-08-08 22:20:27 -0700

    E38

---
+++ 536.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def str2tree(self, S):
+        ix = S.find('(')
+        if ix < 0:
+            return TreeNode(int(S)) if S else None
+
+        bal = 0
+        for jx, u in enumerate(S):
+            if u == '(': bal += 1
+            if u == ')': bal -= 1
+            if jx > ix and bal == 0:
+                break
+
+        root = TreeNode(int(S[:ix]))
+        root.left = self.str2tree(S[ix+1:jx])
+        root.right = self.str2tree(S[jx+2:-1])
+        return root
